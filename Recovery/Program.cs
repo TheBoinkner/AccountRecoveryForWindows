394eae546d895a4abf372affcc7fe799ed77fdd1
Alecz Braden <aleczbraden@gmail.com>
2021-10-20 10:55:34 +0900

    new

--- Recovery/Program.cs
+++ Recovery/Program.cs
@@ -7,13 +7,16 @@ namespace Recovery
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("");
             Menu();
         }
         public static void Menu()
         {
-            Console.WriteLine("");
-            Console.WriteLine("Step 2");
+            Console.WriteLine("Switch cmd with ease of access");
+            Console.WriteLine("Change Password(s)");
+            Console.WriteLine("Activate/Deactivate Accounts");
             Console.WriteLine("Revert");
+            Console.WriteLine("Reboot");
             var tmp = Console.ReadLine();
             _ = int.TryParse(tmp, out int select);
 
@@ -36,7 +39,7 @@ namespace Recovery
                         case 2:
                             try
                             {
-
+                                Password();
                             }
                             catch (Exception e)
                             {
@@ -44,6 +47,26 @@ namespace Recovery
                             }
                             break;
                         case 3:
+                            try
+                            {
+                                Account();
+                            }
+                            catch (Exception e)
+                            {
+                                Console.Write(e);
+                            }
+                            break;
+                        case 4:
+                            try
+                            {
+                                Revert();
+                            }
+                            catch (Exception e)
+                            {
+                                Console.Write(e);
+                            }
+                            break;
+                        case 5:
                             try
                             {
 
@@ -65,10 +88,14 @@ namespace Recovery
         {
             try
             {
-                if (File.Exists("C:\\Windows\\System32\\cmd.exe"))
+                if (File.Exists("C:\\Windows\\System32\\cmd.exe") && File.Exists("C:\\Windows\\System32\\utilman.exe"))
                 {
-
-                    Console.Write("Finished");
+                    File.Copy("C:\\Windows\\System32\\utilman.exe", "C:\\Windows\\System32\\utilman1.exe");
+                    File.Delete("C:\\Windows\\System32\\Utilman.exe");
+                    File.Copy("C:\\Windows\\System32\\cmd.exe", "C:\\Windows\\System32\\utilman.exe");
+                    Console.WriteLine("Finished");
+                    Console.Write("To be able to change passwords you will need to reboot Windows and click the \"ease of access\" button (bottom right of the screen) " +
+                        "and either navigate to the drive and execute this program again to automatically change stuff or do it directly in cmd");
                     Console.ReadKey();
                     Console.Clear();
                     Menu();
@@ -79,7 +106,15 @@ namespace Recovery
                 Console.WriteLine(e);
             }
         }
-        public static void ChangeMenu()
+        public static void Password()
+        {
+
+        }
+        public static void Account()
+        {
+
+        }
+        public static void Revert()
         {
 
         }
