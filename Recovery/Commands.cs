24ed1a62ea8402269e236291724826ff0822108a
Alecz Braden <aleczbraden@gmail.com>
2021-10-21 10:04:56 +0900

    new

---
+++ Recovery/Commands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recovery
+{
+    class Commands
+    {
+        public static Dictionary<string, string> help = new();
+        public static Dictionary<string, Action> mydic = new();
+        public void Dictionary()
+        {
+            help.Add("/help help", "shows commands and what they do");
+            help.Add("/help", "help {COMMAND}");
+            help.Add("/help back", "Goes to the Main Menu");
+            mydic.Add("/back", () => Program.Menu());
+        }
+        public static void SearchDic(string value)
+        {
+            if (help.TryGetValue(value, out string result))
+            {
+                {
+                    Console.Write(result);
+                }
+            }
+            else if (mydic.TryGetValue(value, out var result2))
+            {
+                result2();
+            }
+        }
+    }
+}
--- Recovery/Program.cs
+++ Recovery/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Security;
 using System.Threading;
 
 namespace Recovery
 {
     class Program
     {
-        //Need to add commands to allow users to go back to menu at any time
-        //Maybe redo how I handle changing passwords
         static void Main(string[] args)
         {
             string welcome = "This Application is used to recover a user or data without having to be logged in. You will need to use the \"switch\" at least once in the boot terminal for this to work";
@@ -17,8 +16,9 @@ namespace Recovery
         }
         public static void Menu()
         {
+            Console.WriteLine("To display \"Help\" type /help");
             Console.WriteLine("1. Switch/Revert");
-            Console.WriteLine("2. Change Password");
+            Console.WriteLine("2. Change Password or Create an Account");
             Console.WriteLine("3. Activate/Deactivate Accounts");
             Console.WriteLine("4. Reboot");
             Console.Write("> ");
@@ -44,6 +44,7 @@ namespace Recovery
                         case 2:
                             try
                             {
+                                Console.Clear();
                                 Password();
                             }
                             catch (Exception e)
@@ -64,7 +65,7 @@ namespace Recovery
                         case 4:
                             try
                             {
-                                Process.Start("cmd.exe", $"/c shutdown /r");
+                                Process.Start("cmd.exe", $"/c shutdown /r /t0");
                                 break;
                             }
                             catch (Exception e)
@@ -77,7 +78,7 @@ namespace Recovery
                 catch
                 {
                     Console.Clear();
-                    Console.WriteLine("Sorry chump thats not a vailid input"); 
+                    Console.WriteLine("Sorry chump thats not a vailid input");
                     Console.ReadKey();
                     Menu();
                 }
@@ -121,19 +122,75 @@ namespace Recovery
         {
             try
             {
-                Console.Write("Current Users:");
                 Process.Start("cmd.exe", "/c net user");
                 Thread.Sleep(100);
-                Console.WriteLine("Enter user you want to change password");
-                Console.Write(">");
-                string user = Console.ReadLine();
-                Console.WriteLine("Enter Password");
-                string pass = Console.ReadLine();
-                Process.Start("cmd.exe", $"/c net user {user} {pass}");
+                Console.WriteLine("Enter User you would like to change password");
+                Console.WriteLine("To ADD a user \"add {USER}\"");
+                Console.WriteLine("To Delete a user \"delete {USER}\"");
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                Commands.SearchDic(input);
+                string[] inarray = new string[2];
+                inarray = input.Split(' ');
+                if (inarray[0].ToLower() == "add")
+                {
+                    Process.Start("cmd.exe", $"/c net user {inarray[1]} /add");
+                }
+                if (inarray[0].ToLower() == "delete")
+                {
+                    Process.Start("cmd.exe", $"/c net user {inarray[1]} /delete");
+                }
+                if (inarray.Length == 1)
+                {
+                    SecureString securePwd = new SecureString();
+                    SecureString securePwd2 = new SecureString();
+                    ConsoleKeyInfo key;
+
+                    Console.Write("Enter password: ");
+                    do
+                    {
+                        key = Console.ReadKey(true);
+                        // Ignore any key out of range.
+                        if (((int)key.Key) >= 65 && ((int)key.Key <= 90))
+                        {
+                            // Append the character to the password.
+                            securePwd.AppendChar(key.KeyChar);
+                            Console.Write("*");
+                        }
+                        // Exit if Enter key is pressed.
+                    } while (key.Key != ConsoleKey.Enter);
+                    
+                    Console.Write("Confirm password: ");
+                    do
+                    {
+                        key = Console.ReadKey(true);
+                        // Ignore any key out of range.
+                        if (((int)key.Key) >= 65 && ((int)key.Key <= 90))
+                        {
+                            // Append the character to the password.
+                            securePwd.AppendChar(key.KeyChar);
+                            Console.Write("*");
+                        }
+                        // Exit if Enter key is pressed.
+                    } while (key.Key != ConsoleKey.Enter);
+                    if (securePwd == securePwd2)
+                    {
+                        Process.Start("cmd.exe", $"/c net user {securePwd}");
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Sorry chump thats not a vailid input");
+                    Console.ReadKey();
+                    Password();
+                }
                 Console.WriteLine("Finished");
                 Console.ReadKey();
+                Password();
+                Console.Clear();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.Clear();
                 Console.Write(e);
@@ -145,11 +202,10 @@ namespace Recovery
         {
             try
             {
-                Console.Write("Current Users:");
                 Process.Start("cmd.exe", "/c net user");
                 Thread.Sleep(100);
                 Console.WriteLine("Type {USER} {ENABLE\\DISABLE}");
-                Console.Write(">");
+                Console.Write("> ");
                 string endis = Console.ReadLine();
                 string[] myarray = new string[2];
                 myarray = endis.Split(' ');
@@ -163,6 +219,7 @@ namespace Recovery
                 }
                 Console.WriteLine("Finished");
                 Console.ReadKey();
+                DisEnable();
             }
             catch (Exception e)
             {
